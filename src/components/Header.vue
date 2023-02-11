c3e88b0b839f1172628f0203e2ae79736f9a97f2
Bruno Leandro <bruno.lbs100@gmail.com>
2023-02-11 00:30:03 -0300

    Building header logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-  <header>
-    <nav>
-      <div>
-        <span class="Rails"> Rails </span>
-        <span> Vue </span>
+  <header class="header">
+    <nav class="header__nav">
+      <div class="header__logodiv">
+        <span class="header__logodiv__rails"> Rails </span>
+        <span class="header__logodiv__vue"> Vue </span>
       </div>
     </nav>
   </header>
@@ -11,4 +11,23 @@
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.header {
+  &__nav {
+  }
+  &__logodiv {
+    padding-left: 2rem;
+    font-family: Flamenco;
+    font-size: 4rem;
+    background-color: #2d3238;
+    &__rails {
+      background-color: #2d3238;
+      color: #FF1E00;
+    }
+    &__vue {
+      color: #03C988;
+      background-color: #2d3238;
+    }
+  }
+}
+</style>
